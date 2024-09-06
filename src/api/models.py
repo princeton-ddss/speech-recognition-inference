a613ee5417c127633e893bbd33fd38ff03ac07d5
Colin Swaney <colinswaney@princeton.edu>
2024-09-05 20:15:02 -0400

    Move data types to models.py

--- src/api/models.py
+++ src/api/models.py
@@ -1,16 +1,22 @@
 from pydantic import BaseModel, Field
-from typing import Union
+from typing import Union, Literal
+
+
+class TranscriptionRequest(BaseModel):
+    audio_file: str = Field(examples=["audio.wav"])
+    language: Union[str, None] = Field(default=None, examples=["english"])
+    response_format: Literal["json", "text"] = Field(examples=["json"])
 
 
 class Segment(BaseModel):
     language: str = Field(examples=["english"])
     text: str = Field(examples=["Hello World", "Thanks"])
-    start: int = Field(examples=[0, 3])
-    end: int = Field(examples=[2, 4])
+    start: float = Field(examples=[0, 3])
+    end: float = Field(examples=[2, 4])
 
 
-class TranscriptionOutputs(BaseModel):
-    file: str = Field(examples=["audio.wav"])
+class TranscriptionResponse(BaseModel):
+    audio_file: str = Field(examples=["audio.wav"])
     text: Union[None, str] = Field(default=None, examples=["Hello World"])
     segments: Union[list[Segment], None] = Field(
         default=None,
--- src/data_types/inputs/transcription.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Union, Literal
-
-class TranscriptionInputs(BaseModel):
-    file_name: str = Field(examples = ["sample_data_short.wav"])
-    language: Union[str, None] = Field(default = None, examples = ["english"])
-    response_format: Literal["json", "text"] = Field(examples = [
-        "json"])
