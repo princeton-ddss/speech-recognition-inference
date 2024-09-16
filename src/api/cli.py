d5be50f17dcb9504bfa0b060c06fc90192057c07
fjying <jf3375@princeton.edu>
2024-09-16 15:04:08 -0400

    Drop if __name__ == "__main__" to enable speech_recognition_launcher command defined in pyproject.toml

--- src/api/cli.py
+++ src/api/cli.py
@@ -1,11 +1,10 @@
 import uvicorn
 
 def launch():
-    from __init__ import config
+    from .__init__ import config
 
-    if __name__ == '__main__':
-        uvicorn.run(
-            "main:app", port=config.port, host=config.host,
-            reload=config.auto_reload
-        )
+    uvicorn.run(
+        "api.main:app", port=config.port, host=config.host,
+        reload=config.auto_reload
+    )
 
