f0eb126771118f70040b530ac2d3e4b93c34fa7c
fjying <jf3375@princeton.edu>
2024-09-16 14:40:54 -0400

    Add if __name__ = '__main__' for interactive debugging

--- src/api/cli.py
+++ src/api/cli.py
@@ -1,9 +1,11 @@
 import uvicorn
 
-
 def launch():
-    from api import config
+    from __init__ import config
 
-    uvicorn.run(
-        "api:app", port=config.port, host=config.host, reload=config.auto_reload
-    )
+    if __name__ == '__main__':
+        uvicorn.run(
+            "main:app", port=config.port, host=config.host,
+            reload=config.auto_reload
+        )
+
