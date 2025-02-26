c67b006817a910d9e895a5864563e2d39a22a2eb
fjying <jf3375@princeton.edu>
2025-02-26 10:42:59 -0500

    Filter not meaningful symbols from results

--- src/batch_processing/parsing.py
+++ src/batch_processing/parsing.py
@@ -12,14 +12,20 @@ def parse_string_into_result(input_string, language):
     # Convert segments to dictionary
     result = {}
     result["language"] = language
-    result["text"] = ""
-    result["chunks"] = [0] * len(matches)
-    for idx, match in enumerate(matches):
-        result["text"] += match[1]
-        segment = {}
-        segment["timestamp"] = (float(match[0]), float(match[2]))
-        segment["text"] = match[1]
-        result["chunks"][idx] = segment
+    if not matches:
+        # if matches are empty, no segments
+        # Remove random timestamps
+        result["text"] = re.sub(r'<\|.*?\|> |!\s', '', input_string).strip()
+        result["chunks"] = [{"timestamp": (0, 30), "text": input_string}]
+    else:
+        result["text"] = ""
+        result["chunks"] = [0] * len(matches)
+        for idx, match in enumerate(matches):
+            result["text"] += match[1]
+            segment = {}
+            segment["timestamp"] = (float(match[0]), float(match[2]))
+            segment["text"] = match[1]
+            result["chunks"][idx] = segment
     return result
 
 
@@ -56,12 +62,14 @@ def parse_results_to_csv(result, output_dir, output_name):
 
 
 input_string = (
-    "<|0.00|> and the 16 years that we've been married.<|2.00|><|2.00|> Have you one"
-    " time told off that you liked him?<|5.00|><|5.00|> Not in those exact"
+    "<|0.00|> an ofd the 16 years that we've been married.<|2.00|><|2.00|> Have you one"
+    " time toldf that you liked him?<|5.00|><|5.00|> Not in those exact"
     " words.<|7.00|><|7.00|> No.<|8.00|><|8.00|> No.<|9.00|><|9.00|> Not in any words,"
     " Dad.<|10.00|><|10.00|> He said that makes me feel.<|12.00|><|13.00|> You've never"
     " told your son that you love him.<|16.00|>"
 )
 
+input_string=" ! Good morning. I now call this meeting of the Abilene City Council order is 830 a.m. I'm going to introduce here in a moment but I'm going to ask Councilman McAllister if he would lead us in our invocation. Let's have a word. Dearly Father, we are very blessed to be here today. We thank you for this opportunity and it is as we know an opportunity to serve you in so many different ways. We pray this morning that we"
 result = parse_string_into_result(input_string, "en")
 print(result)
+
