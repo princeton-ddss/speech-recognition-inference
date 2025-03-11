022f5bdca587c311e6df95de20c6c452c8378543
Colin Swaney <colinswaney@gmail.com>
2025-03-11 15:48:02 -0400

    Fix default `model_dir`

--- src/config.py
+++ src/config.py
@@ -8,11 +8,9 @@ from pydantic import ConfigDict
 @dataclass
 class SpeechRecognitionInferenceConfig:
     model_config = ConfigDict(protected_namespaces=())
+    model_dir: Optional[str] = os.path.expanduser("~")
     model_id: Optional[str] = "openai/whisper-tiny"
     revision: Optional[str] = None
-    model_dir: Optional[str] = os.getenv(
-        "SRI_MODEL_DIR", "/Users/jf3375/.blackfish/models"
-    )
     port: int = os.getenv("SRI_PORT", 8080)
     host: str = os.getenv("SRI_HOST", "0.0.0.0")
     auto_reload: bool = False
